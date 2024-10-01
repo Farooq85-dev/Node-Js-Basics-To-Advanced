08c6dbc16b96d928f7ce3df061b7042597860de0
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-10-01 11:28:33 +0500

    npm modules

---
+++ NpmModules/modules.js
@@ -0,0 +1,20 @@
+// NPM Modules
+// pm is a package manager for the JavaScript programming language maintained by npm, Inc., a subsidiary of GitHub. npm is the default package manager for the JavaScript runtime environment Node.js and is included as a recommended feature in the Node.js installer.
+
+// https://www.npmjs.com/
+
+// Run npm init command to create a pkg.json file
+// If you want to skip quesion the put flag after npm init -y or --yes
+
+// Run this command to install any package as dependency npm i or install <package-name>
+// Run this command to install any package as development dependency npm i or install <package-name> -D
+// Run this command to uninstall any package as developent dependency npm uninstall or un <package-name> -D or --save-dev
+// Run this command to update all version of packages in package.json npm update
+
+// "^8.2.3."
+//Here, 8 represent Major Version
+//Here, 2 represent Minor Version
+//Here, 3 represent Patch Version
+
+// if you remove caret symbol it means every one have to install this specific version "8.2.3."
+// if you put only asteric symbol it means every time have to update this package "8.2.3." Note: it is dangerous.
